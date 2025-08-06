8e8e36cfdaf0c6b65642f0e3c545ccf9850d3801
AryaTabani <a.tabani82@gmail.com>
2025-08-06 19:16:58 +0330

    implementing main

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	db "example.com/m/v2/DB"
+	"example.com/m/v2/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db.InitDB()
+	router := gin.Default()
+
+	router.POST("/register", controllers.RegisterHandler())
+	router.POST("/login", controllers.LoginHandler())
+
+
+	
+	router.Run(":8080")
+}
