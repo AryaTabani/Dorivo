5f8834d95f0c65af2673608184d98cdac6025d64
AryaTabani <a.tabani82@gmail.com>
2025-08-08 17:00:36 +0330

    debug for vercel

--- DB/db.go
+++ DB/db.go
@@ -25,23 +25,34 @@ func InitDB() {
 }
 
 func createTables() {
+	createTenantsTable := `
+	CREATE TABLE IF NOT EXISTS tenants (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT NOT NULL UNIQUE, 
+		config TEXT NOT NULL
+	);`
+
+	_, err := DB.Exec(createTenantsTable)
+	if err != nil {
+		panic("Failed to create tenants table: " + err.Error())
+	}
 
 	createUsersTable := `
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    tenant_id TEXT NOT NULL, 
-    full_name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    mobile_number TEXT,
-    password_hash TEXT NOT NULL,
-    date_of_birth TEXT,
-    FOREIGN KEY (tenant_id) REFERENCES tenants(id) ON DELETE CASCADE,
-    UNIQUE (tenant_id, email)
-);`
+	CREATE TABLE IF NOT EXISTS users (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		tenant_id TEXT NOT NULL, 
+		full_name TEXT NOT NULL,
+		email TEXT NOT NULL,
+		mobile_number TEXT,
+		password_hash TEXT NOT NULL,
+		date_of_birth TEXT,
+		FOREIGN KEY (tenant_id) REFERENCES tenants(name) ON DELETE CASCADE,
+		UNIQUE (tenant_id, email)
+	);`
 
-	_, err := DB.Exec(createUsersTable)
+	_, err = DB.Exec(createUsersTable)
 	if err != nil {
-		panic("Failed to create users table")
+		panic("Failed to create users table: " + err.Error())
 	}
 
 	createPasswordResetsTable := `
@@ -55,18 +66,7 @@ CREATE TABLE IF NOT EXISTS users (
 
 	_, err = DB.Exec(createPasswordResetsTable)
 	if err != nil {
-		panic("Failed to create password_reset_tokens table")
-	}
-	createTenantsTable := `
-    CREATE TABLE IF NOT EXISTS tenants (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL UNIQUE, 
-        config TEXT NOT NULL
-    );`
-
-	_, err = DB.Exec(createTenantsTable)
-	if err != nil {
-		panic("Failed to create tenants table: " + err.Error())
+		panic("Failed to create password_reset_tokens table: " + err.Error())
 	}
 }
 func createDefaultTenant() {
