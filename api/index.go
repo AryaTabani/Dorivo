9b576a09bec03ca698d995aaf7654b43f35bc91a
AryaTabani <a.tabani82@gmail.com>
2025-09-06 13:22:50 +0330

    updating api

--- api/index.go
+++ api/index.go
@@ -5,6 +5,7 @@ import (
 
 	db "github.com/AryaTabani/Dorivo/DB"
 	"github.com/AryaTabani/Dorivo/controllers"
+	"github.com/AryaTabani/Dorivo/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,6 +17,12 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	router.GET("/tenant/:tenantId", controllers.GetTenantConfigHandler())
 	router.POST("/register", controllers.RegisterHandler())
 	router.POST("/login", controllers.LoginHandler())
-	
+	userAuthGroup := router.Group("/")
+	userAuthGroup.Use(middleware.AuthMiddleware())
+	{
+		userAuthGroup.GET("/orders", controllers.GetMyOrdersHandler())
+		// userAuthGroup.POST("/orders/:orderId/cancel", controllers.CancelOrderHandler())
+		// userAuthGroup.POST("/orders/:orderId/review", controllers.LeaveReviewHandler())
+	}
 	router.ServeHTTP(w, r)
 }
