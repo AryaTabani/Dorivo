1d6f9cc686f626a1b127a43f55bf51506845fa7b
Arya Tabani <a.tabani82@gmail.com>
2025-09-24 17:27:40 +0330

    adding new APIs

--- api/index.go
+++ api/index.go
@@ -15,11 +15,23 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	router := gin.Default()
 
 	router.GET("/tenant/:tenantId", controllers.GetTenantConfigHandler())
-	router.POST("/register", controllers.RegisterHandler())
-	router.POST("/login", controllers.LoginHandler())
+	router.POST("/:tenantId/register", controllers.RegisterHandler())
+	router.POST("/:tenantId/login", controllers.LoginHandler())
+
 	userAuthGroup := router.Group("/")
 	userAuthGroup.Use(middleware.AuthMiddleware())
 	{
+		userAuthGroup.GET("/profile", controllers.GetProfileHandler())
+		userAuthGroup.PUT("/profile", controllers.UpdateProfileHandler())
+
+		userAuthGroup.GET("/addresses", controllers.GetAddressesHandler())
+		userAuthGroup.POST("/addresses", controllers.AddAddressHandler())
+		userAuthGroup.DELETE("/addresses/:addressId", controllers.DeleteAddressHandler())
+
+		userAuthGroup.GET("/payment-methods", controllers.GetPaymentMethodsHandler())
+		userAuthGroup.POST("/payment-methods", controllers.AddPaymentMethodHandler())
+		userAuthGroup.DELETE("/payment-methods/:methodId", controllers.DeletePaymentMethodHandler())
+
 		userAuthGroup.GET("/orders", controllers.GetMyOrdersHandler())
 		userAuthGroup.POST("/orders/:orderId/cancel", controllers.CancelOrderHandler())
 		userAuthGroup.POST("/orders/:orderId/review", controllers.LeaveReviewHandler())
--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 
 func main() {
 	db.InitDB()
+
 	router := gin.Default()
+
 	router.GET("/tenant/:tenantId", controllers.GetTenantConfigHandler())
 	router.POST("/:tenantId/register", controllers.RegisterHandler())
 	router.POST("/:tenantId/login", controllers.LoginHandler())
@@ -17,10 +19,20 @@ func main() {
 	userAuthGroup := router.Group("/")
 	userAuthGroup.Use(middleware.AuthMiddleware())
 	{
+		userAuthGroup.GET("/profile", controllers.GetProfileHandler())
+		userAuthGroup.PUT("/profile", controllers.UpdateProfileHandler())
+
+		userAuthGroup.GET("/addresses", controllers.GetAddressesHandler())
+		userAuthGroup.POST("/addresses", controllers.AddAddressHandler())
+		userAuthGroup.DELETE("/addresses/:addressId", controllers.DeleteAddressHandler())
+
+		userAuthGroup.GET("/payment-methods", controllers.GetPaymentMethodsHandler())
+		userAuthGroup.POST("/payment-methods", controllers.AddPaymentMethodHandler())
+		userAuthGroup.DELETE("/payment-methods/:methodId", controllers.DeletePaymentMethodHandler())
+
 		userAuthGroup.GET("/orders", controllers.GetMyOrdersHandler())
 		userAuthGroup.POST("/orders/:orderId/cancel", controllers.CancelOrderHandler())
 		userAuthGroup.POST("/orders/:orderId/review", controllers.LeaveReviewHandler())
-
 	}
 
 	router.Run(":8080")
