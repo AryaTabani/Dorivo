5c50a803a5128dd3fbb59c08e1084cabca0f2ff1
Arya Tabani <a.tabani82@gmail.com>
2025-09-26 13:02:58 +0330

    adding contact info to tenants

--- models/tenant.go
+++ models/tenant.go
@@ -15,9 +15,9 @@ const (
 )
 
 type ThemeColors struct {
-	Primary   string `json:"primary"`
-	Primary2  string `json:"primary2"`
-	Secondary string `json:"secondary"`
+	Primary    string `json:"primary"`
+	Primary2   string `json:"primary2"`
+	Secondary  string `json:"secondary"`
 	Secondary2 string `json:"secondary2"`
 }
 
@@ -28,11 +28,20 @@ type TenantConfig struct {
 	MultiTheme   bool            `json:"multiTheme"`
 	DefaultTheme Theme           `json:"defaultTheme"`
 	ThemeColors  ThemeColors     `json:"themeColors"`
-	Features     json.RawMessage `json:"features"` 
+	Features     json.RawMessage `json:"features"`
+	ContactInfo  ContactInfo     `json:"contactInfo"`
 }
 
 type Tenant struct {
 	ID     int64
 	Name   string
 	Config TenantConfig
+}
+
+type ContactInfo struct {
+	CustomerService string `json:"customerService"`
+	Website         string `json:"website"`
+	Whatsapp        string `json:"whatsapp"`
+	Facebook        string `json:"facebook"`
+	Instagram       string `json:"instagram"`
 }
