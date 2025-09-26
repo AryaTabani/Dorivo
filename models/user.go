ac6f2367dcad07a6a25d540548c3be78be6e5d09
Arya Tabani <a.tabani82@gmail.com>
2025-09-26 18:18:20 +0330

    updating user model for notifications preferences

--- models/user.go
+++ models/user.go
@@ -1,14 +1,15 @@
 package models
 
 type User struct {
-	ID            int64  `json:"id"`
-	TenantID      string `json:"-"`
-	Full_name     string `json:"full_name"`
-	Email         string `json:"email"`
-	Mobile_number string `json:"mobile_number"`
-	Date_of_birth string `json:"date_of_birth"`
-	Avatar_url    string `json:"avatar_url"`
-	Password_hash string `json:"-"`
+	ID                      int64                   `json:"id"`
+	TenantID                string                  `json:"-"`
+	Full_name               string                  `json:"full_name"`
+	Email                   string                  `json:"email"`
+	Mobile_number           string                  `json:"mobile_number"`
+	Date_of_birth           string                  `json:"date_of_birth"`
+	Avatar_url              string                  `json:"avatar_url"`
+	NotificationPreferences NotificationPreferences `json:"notification_preferences"`
+	Password_hash           string                  `json:"-"`
 }
 
 type RegisterPayload struct {
@@ -33,4 +34,18 @@ type UpdateProfilePayload struct {
 	Mobile_number string `json:"mobile_number"`
 	Date_of_birth string `json:"date_of_birth"`
 	Avatar_url    string `json:"avatar_url"`
+}
+
+type NotificationPreferences struct {
+	GeneralNotifications bool `json:"general_notifications"`
+	Sound                bool `json:"sound"`
+	Vibrate              bool `json:"vibrate"`
+	SpecialOffers        bool `json:"special_offers"`
+	Payments             bool `json:"payments"`
+	Cashback             bool `json:"cashback"`
+}
+
+type ChangePasswordPayload struct {
+	CurrentPassword string `json:"current_password" binding:"required"`
+	NewPassword     string `json:"new_password" binding:"required,min=8"`
 }
