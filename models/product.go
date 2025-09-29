1ee099ae40c4333107be55d119c17eeb4521c561
Arya Tabani <a.tabani82@gmail.com>
2025-09-29 17:50:31 +0330

    updating product model for product customization

--- models/product.go
+++ models/product.go
@@ -1,19 +1,31 @@
 package models
 
 type Product struct {
-	ID           int64    `json:"id"`
-	TenantID     string   `json:"-"`
-	Name         string   `json:"name"`
-	Description  string   `json:"description"`
-	Price        float64  `json:"price"`
-	Rating       float64  `json:"rating"`
-	ImageURL     string   `json:"image_url"`
-	MainCategory string   `json:"main_category"`
-	Tags         []string `json:"tags,omitempty"`
+	ID           int64         `json:"id"`
+	TenantID     string        `json:"-"`
+	Name         string        `json:"name"`
+	Description  string        `json:"description"`
+	Price        float64       `json:"price"`
+	Rating       float64       `json:"rating"`
+	ImageURL     string        `json:"image_url"`
+	MainCategory string        `json:"main_category"`
+	Tags         []string      `json:"tags,omitempty"`
+	OptionGroups []OptionGroup `json:"option_groups,omitempty"`
 }
 
 type Tag struct {
 	ID           int64  `json:"id"`
 	Name         string `json:"name"`
 	MainCategory string `json:"main_category"`
+}
+type Option struct {
+	ID            int64   `json:"id"`
+	Name          string  `json:"name"`
+	PriceModifier float64 `json:"price_modifier"`
+}
+type OptionGroup struct {
+	ID            int64    `json:"id"`
+	Name          string   `json:"name"`
+	SelectionType string   `json:"selection_type"`
+	Options       []Option `json:"options"`
 }
