4afa4de3ead5ae0a8c66da201f3d668963a1661e
Arya Tabani <a.tabani82@gmail.com>
2025-09-30 17:34:58 +0330

    adding featured,bestseller,recomended nooleans to model

--- models/product.go
+++ models/product.go
@@ -1,16 +1,19 @@
 package models
 
 type Product struct {
-	ID           int64         `json:"id"`
-	TenantID     string        `json:"-"`
-	Name         string        `json:"name"`
-	Description  string        `json:"description"`
-	Price        float64       `json:"price"`
-	Rating       float64       `json:"rating"`
-	ImageURL     string        `json:"image_url"`
-	MainCategory string        `json:"main_category"`
-	Tags         []string      `json:"tags,omitempty"`
-	OptionGroups []OptionGroup `json:"option_groups,omitempty"`
+	ID            int64         `json:"id"`
+	TenantID      string        `json:"-"`
+	Name          string        `json:"name"`
+	Description   string        `json:"description"`
+	Price         float64       `json:"price"`
+	Rating        float64       `json:"rating"`
+	ImageURL      string        `json:"image_url"`
+	MainCategory  string        `json:"main_category"`
+	DiscountPrice *float64      `json:"discount_price,omitempty"`
+	IsFeatured    bool          `json:"is_featured"`
+	IsRecommended bool          `json:"is_recommended"`
+	Tags          []string      `json:"tags,omitempty"`
+	OptionGroups  []OptionGroup `json:"option_groups,omitempty"`
 }
 
 type Tag struct {
