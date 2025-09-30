43078588c16a0ef4dbd0fb47da4cd32d37ccb31c
Arya Tabani <a.tabani82@gmail.com>
2025-09-30 17:36:04 +0330

    adding service for featured,best seller and recomended products

--- services/productService.go
+++ services/productService.go
@@ -11,6 +11,8 @@ import (
 
 var ErrProductNotFound = errors.New("product not found")
 
+const BEST_SELLER_LIMIT = 10
+
 func SearchProducts(ctx context.Context, tenantID string, filters map[string][]string) ([]models.Product, error) {
 	return repository.SearchProducts(ctx, tenantID, filters)
 }
@@ -27,4 +29,22 @@ func GetProductDetails(ctx context.Context, tenantID string, productID int64) (*
 		return nil, err
 	}
 	return product, nil
+}
+func GetBestSellers(ctx context.Context, tenantID string) ([]models.Product, error) {
+	return repository.GetBestSellers(ctx, tenantID, BEST_SELLER_LIMIT)
+}
+
+func GetFeaturedProduct(ctx context.Context, tenantID string) (*models.Product, error) {
+	product, err := repository.GetFeaturedProduct(ctx, tenantID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrProductNotFound
+		}
+		return nil, err
+	}
+	return product, nil
+}
+
+func GetRecommendedProducts(ctx context.Context, tenantID string) ([]models.Product, error) {
+	return repository.GetRecommendedProducts(ctx, tenantID)
 }
