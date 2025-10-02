b142b29f0a8defd8f0e93f42cfedeb50b5165a58
Arya Tabani <a.tabani82@gmail.com>
2025-10-02 14:51:24 +0330

    adding admin model

---
+++ models/admin.go
@@ -0,0 +1,12 @@
+package models
+
+type ProductPayload struct {
+	Name          string   `json:"name" binding:"required"`
+	Description   string   `json:"description"`
+	Price         float64  `json:"price" binding:"required"`
+	ImageURL      string   `json:"image_url"`
+	MainCategory  string   `json:"main_category" binding:"required"`
+	DiscountPrice *float64 `json:"discount_price"`
+	IsFeatured    bool     `json:"is_featured"`
+	IsRecommended bool     `json:"is_recommended"`
+}
