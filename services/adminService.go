1f28904d3316a2d2b904d76303f03b0b7f03f0c8
Arya Tabani <a.tabani82@gmail.com>
2025-10-02 22:03:57 +0330

    adding services for admin new accesses

--- services/adminService.go
+++ services/adminService.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"errors"
 
 	"github.com/AryaTabani/Dorivo/models"
 	"github.com/AryaTabani/Dorivo/repository"
@@ -21,4 +22,26 @@ func DeleteProduct(ctx context.Context, tenantID string, productID int64) error 
 
 func UpdateTenantConfig(ctx context.Context, tenantID string, payload *models.TenantConfig) error {
 	return repository.UpdateTenantConfig(ctx, tenantID, payload)
+}
+func GetTenantOrders(ctx context.Context, tenantID string, status string) ([]models.Order, error) {
+	if status == "" {
+		status = "Active"
+	}
+	return repository.GetOrdersByTenantID(ctx, tenantID, status)
+}
+func UpdateOrderStatus(ctx context.Context, tenantID string, orderID int64, newStatus string) error {
+	RowsAffected, err := repository.AdminUpdateOrderStatus(ctx, tenantID, orderID, newStatus)
+	if err != nil {
+		return err
+	}
+	if RowsAffected == 0 {
+		return errors.New("order not found or you do not have permission to update it")
+	}
+	return nil
+}
+func GetTenantCustomers(ctx context.Context, tenantID string) ([]models.User, error) {
+	return repository.GetUsersByTenantID(ctx, tenantID)
+}
+func GetDashboardStats(ctx context.Context, tenantID string) (*models.DashboardStats, error) {
+	return repository.GetTenantDashboardStats(ctx, tenantID)
 }
