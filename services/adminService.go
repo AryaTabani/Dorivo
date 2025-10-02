a66c843e9d294855181440578179eefffbb977e3
Arya Tabani <a.tabani82@gmail.com>
2025-10-02 14:52:26 +0330

    adding functions to create update and delete products

---
+++ services/adminService.go
@@ -0,0 +1,20 @@
+package services
+
+import (
+	"context"
+
+	"github.com/AryaTabani/Dorivo/models"
+	"github.com/AryaTabani/Dorivo/repository"
+)
+
+func CreateProduct(ctx context.Context, tenantID string, payload *models.ProductPayload) (int64, error) {
+	return repository.CreateProduct(ctx, tenantID, payload)
+}
+
+func UpdateProduct(ctx context.Context, tenantID string, productID int64, payload *models.ProductPayload) error {
+	return repository.UpdateProduct(ctx, tenantID, productID, payload)
+}
+
+func DeleteProduct(ctx context.Context, tenantID string, productID int64) error {
+	return repository.DeleteProduct(ctx, tenantID, productID)
+}
