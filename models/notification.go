e74b268afb18a7d857848890616b01701baba224
Arya Tabani <a.tabani82@gmail.com>
2025-10-06 01:27:59 +0330

    updating notification model for swagger

--- models/notification.go
+++ models/notification.go
@@ -1,19 +1,18 @@
 package models
 
 import (
-	"encoding/json"
 	"time"
 )
 
 type Notification struct {
-	ID        int64           `json:"id"`
-	UserID    int64           `json:"-"`
-	Title     string          `json:"title"`
-	Type      string          `json:"type"`
-	Content   string          `json:"content"`
-	IsRead    bool            `json:"is_read"`
-	Metadata  json.RawMessage `json:"metadata,omitempty"`
-	CreatedAt time.Time       `json:"created_at"`
+	ID        int64         `json:"id"`
+	UserID    int64         `json:"-"`
+	Title     string        `json:"title"`
+	Type      string        `json:"type"`
+	Content   string        `json:"content"`
+	IsRead    bool          `json:"is_read"`
+	Metadata  RawJSONObject `json:"metadata,omitempty"`
+	CreatedAt time.Time     `json:"created_at"`
 }
 
 type MarkReadPayload struct {
