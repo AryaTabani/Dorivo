8abb2e5971a6677810f89b2795d3d21d07b76474
Arya Tabani <a.tabani82@gmail.com>
2025-10-06 01:28:23 +0330

    updating super admin model

--- models/superadmin.go
+++ models/superadmin.go
@@ -10,3 +10,8 @@ type SuperAdminLoginPayload struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required"`
 }
+
+type CreateTenantPayload struct {
+	Name   string       `json:"name" binding:"required"`
+	Config TenantConfig `json:"config" binding:"required"`
+}
